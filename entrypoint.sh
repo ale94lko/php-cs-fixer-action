d4b833f9d8d783fdce391b63c3915d606715c356
Alejandro <47539231+ale94lko@users.noreply.github.com>
2022-07-19 11:36:16 -0400

    Create entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+PHP_FULL_VERSION=$(php -r 'echo phpversion();')
+echo "PHP Version : ${PHP_FULL_VERSION}"
+
+if [ -z "$1" ];
+then
+	DIR="."
+else
+	DIR="$1"
+fi
+
+echo "# Running PHP Code Style Fixer on ${DIR}"
+
+if [ ! -d "${DIR}" ] && [ ! -f "${DIR}" ];
+then
+	echo "\nInvalid file or directory path: ${DIR}\n\n"
+	exit 2
+fi
+
+php -d memory_limit=-1 /php-cs-fixer fix --dry-run -v --show-progress=dots --config=/github/workspace/.php_cs --allow-risky=yes --using-cache=no --diff-format=udiff --path-mode=intersection "${DIR}"
+	
